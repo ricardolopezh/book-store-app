01b4dbf4b8873fa80ec7124febd94daa17b77018
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-23 15:25:57 -0500

    Update navbar menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stores
   get '/newstore', to: 'stores#new'
+  get '/stores', to: 'store#index'
   root 'stores#index'
 end
