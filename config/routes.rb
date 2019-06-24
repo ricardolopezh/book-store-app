7fb0d60049b40c6683ef7db5bef99304f2eb992b
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 15:07:53 -0500

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stores
   resources :books
-  get '/newstore', to: 'stores#new'
-  get '/stores', to: 'store#index'
   get '/home', to: 'static_pages#home'
   root 'static_pages#home'
 end
