ce077ece9983265bbdb895aaa6ccb370f371bc81
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 12:42:38 -0500

    Add books controller with index and new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stores
+  resources :books
   get '/newstore', to: 'stores#new'
   get '/stores', to: 'store#index'
   get '/home', to: 'static_pages#home'
