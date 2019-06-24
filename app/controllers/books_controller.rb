b2eaf19d34b553fcbcec6990f7329b5e4179c3dd
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 13:45:40 -0500

    Add edit button to books view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,21 @@ class BooksController < ApplicationController
 
   def new
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update_attributes(book_params)
+    flash[:primary] = "Book information updated"
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :year)
+  end
 end
