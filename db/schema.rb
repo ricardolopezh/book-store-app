746199bcff1f1165fb8f2a8ebd04445cc3700aec
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 12:53:35 -0500

    Update books view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190622181926) do
+ActiveRecord::Schema.define(version: 20190624174625) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "codename"
