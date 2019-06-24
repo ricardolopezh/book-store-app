b4b178d1daccb386de93ffd314b1f4f923372e6b
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 12:19:33 -0500

    Add signup error messages

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,6 @@
 class Store < ApplicationRecord
-  validates :codename, presence: true, length: { maximum: 6 }
+  validates :codename, presence: true, length: { maximum: 6 },
+                       uniqueness: { case_sensitive: false }
   validates :address, presence: true, length: { maximum: 120 }
   validates :phone, presence: true, length: { maximum: 15 }
 end
