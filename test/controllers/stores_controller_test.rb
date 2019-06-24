bab5173c8289d0868fb9c0125eb76f00454b3cdd
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-24 15:35:14 -0500

    Add 3 tests to stores_controller

--- test/controllers/stores_controller_test.rb
+++ test/controllers/stores_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StoresControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+  	get stores_path
+  	assert_response :success
+  end
+
+  test "should get new" do
+  	get new_store_path
+  	assert_response :success
+  end
+
+  test "should get show" do
+  	get stores_path
+  	assert_response :success
+  end
 end
