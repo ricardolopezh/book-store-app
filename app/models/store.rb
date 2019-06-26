571b9adaaa0782808a9d2bed9ff013a624db0131
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-26 11:35:26 -0500

    Fix errors

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
-  has_and_belongs_to_many :stores
+  has_and_belongs_to_many :books
   validates :codename, presence: true, length: { maximum: 6 },
                        uniqueness: { case_sensitive: false }
   validates :address, presence: true, length: { maximum: 120 }
