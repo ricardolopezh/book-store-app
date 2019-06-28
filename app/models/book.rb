dc7be45ec173749d4e867134f6680799806a46c3
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-27 21:42:22 -0500

    Update book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :store
-  validates :title, presence: true, length: { maximum: 60 },
-                       uniqueness: { case_sensitive: false }
+  validates :title, presence: true, length: { maximum: 80 }
   validates :author, presence: true, length: { maximum: 80 }
   validates :year, presence: true, length: { maximum: 4 }
 end
