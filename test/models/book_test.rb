79ce3560511b608d84f1760996922da3db16c388
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-27 23:59:16 -0500

    Add book model tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = Book.new(title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951, quantity: 100, store_id: 1)
+  end
+
+  test "title should be present" do
+  	@book.title = "   "
+  	assert_not @book.valid?
+  end
+
+  test "author should be present" do
+  	@book.author = "   "
+  	assert_not @book.valid?
+  end
+
+  test "year should be present" do
+  	@book.year = "   "
+  	assert_not @book.valid?
+  end
+
+  test "title should not be too long" do
+    @book.title = "a" * 81
+    assert_not @book.valid?
+  end
+
+  test "author should not be too long" do
+    @book.author = "a" * 81
+    assert_not @book.valid?
+  end
+
+  test "year should not be too long" do
+    @book.year = 1 * 5
+    assert_not @book.valid?
+  end
 end
