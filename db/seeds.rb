2b1d5262b7cccf69de278d0734db1276e5022808
Ricardo Lopez <r.lopez@ukko.mx>
2019-06-28 13:03:49 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
+15.times do
   Store.create([{
     codename: 'MTY0' + rand(1..99).to_s, 
     address: Faker::Address.street_address, 
@@ -13,10 +13,18 @@
     }])
 end
 
-10.times do
+15.times do
 	Book.create([{
     title: Faker::Book.title,
     author: Faker::Book.author,
     year: rand(1440..2019)
+  }])
+end
+
+15.times do
+  Assignment.create([{
+    store_id: rand(1..10),
+    book_id: rand(1..10),
+    quantity: rand(1..1000)
   }])
 end
